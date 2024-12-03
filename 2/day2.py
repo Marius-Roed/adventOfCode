070e4f9e79d4fe62d1f18bc29ad043382e3cf7f0
Marius-Roed <mwroed@gmail.com>
2024-12-03 22:24:04 +0100

    Day 2 | IDFK what am doin

--- 2/day2.py
+++ 2/day2.py
@@ -43,17 +43,43 @@ def prob1():
 
 def prob2():
     safe = 0
+    first = True
 
     for x in range(len(splits) - 1):
+        if not first:
+            continue
         line = splits[x]
+        searching = False
         numbers = []
+        first = True
         for i in range(len(line.split()) - 1):
-            first = True
             char = line.split()
             temp = compare(char[i], char[i+1])
-            if first and not (temp < -3 or temp == 0 or temp > 3) and temp is not None:
+            if first and temp < -3 or temp == 0 or temp > 3:
+                numbers.append(temp)
                 first = False
                 continue
+            elif not (temp < -3 or temp == 0 or temp > 3):
+                numbers.append(temp)
+                continue
+
+        if numbers[0] < 0:
+            for num in numbers:
+                if num > 0:
+                    searching = True
+                    continue
+        else:
+            for num in numbers:
+                if num < 0:
+                    searching = True
+                    continue
+        if searching:
+            first = True
+            continue
+
+        safe = safe + 1
+
+    print(safe)
 
 
 if __name__ == "__main__":
